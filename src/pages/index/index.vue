49d6325b1c6618bc563afd0d02b3001f45808646
webwyb <beijingwyb@163.com>
2019-02-28 20:24:13 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="card">
+    <div class="header">段子头部</div>
+    <div class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cupiditate, ducimus eum hic nemo nostrum obcaecati repellendus velit. Consectetur culpa dicta dignissimos est exercitationem modi molestias nulla officia quibusdam soluta!</div>
+    <div class="footer">
+      <div>作者：吴延彬 | 2018-02-21</div>
+      <div><icon type="success" size="20" /><icon type="success" size="20" /></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme',
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: 'http://mpvue.com/assets/logo.png'
+      }
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test'}
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+  .card{
+    border: 1px solid red;
+    border-radius: 2%;
+    display: flex;
+    flex-direction: column;
+  }
+  .header{
+    border-bottom: 2px solid silver;
+  }
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+.all{
+  width:7.5rem;
+  height:1rem;
+  background-color:blue;
+}
+.all:after{
+  display:block;
+  content:'';
+  clear:both;
+}
+.left{
+  float:left;
+  width:3rem;
+  height:1rem;
+  background-color:red;
+}
+
+.right{
+  float:left;
+  width:4.5rem;
+  height:1rem;
+  background-color:green;
+}
+</style>
