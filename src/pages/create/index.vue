5739d0fcf9d34c92d94a846ef5e1c4d658a9a216
webwyb <beijingwyb@163.com>
2019-03-21 20:26:31 +0800

    feat: 编辑页面

---
+++ src/pages/create/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="cu-form-group">
+      <textarea placeholder="多行文本输入框"
+                maxlength="200"
+                show-confirm-bar="true"
+                adjust-position="true"
+                placeholder-class='placeholder'
+      ></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index",
+    data() {
+    },
+    mounted() {
+      // console.log("上一个页面传来的数据", this.$root.$mp);
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
