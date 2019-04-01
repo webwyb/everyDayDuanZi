f2dd65b59473f98619493d4f4add2754c1610c7b
webwyb <beijingwyb@163.com>
2019-04-01 11:39:16 +0800

    feat: 创作

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from "vue";
 import App from "./App";
-import "./global.css";
+import "./style/reset.css";
+import "./style/global.css";
 
 Vue.config.productionTip = false;
 App.mpType = "app";
 
 // 引用api 文件
-import http from './utils/request'
+import http from "./utils/request";
 // 将API绑定为全局
-Vue.prototype.$http = http
+Vue.prototype.$http = http;
 
 const app = new Vue(App);
 app.$mount();
--- src/pages/create/index.vue
+++ src/pages/create/index.vue
@@ -5,11 +5,11 @@
       <scroll-view scroll-x class="bg-green nav text-center">
         <view class='flex text-center'>
           <view :class="['0' === TabCur ? 'text-white cur' : '','cu-item', 'flex-sub' ]" @click='tabSelect' data-id="0">
-            <text class='icon-camerafill'></text>
+            <text class='icon-upstagefill'></text>
             文字
           </view>
           <view :class="['1' === TabCur ? 'text-white cur' : '','cu-item', 'flex-sub' ]" @click='tabSelect' data-id="1">
-            <text class='icon-upstagefill'></text>
+            <text class='icon-camerafill'></text>
             图片
           </view>
         </view>
@@ -27,24 +27,31 @@
                       v-model="content"
                       @change="handleTextAreaChange"
             ></textarea>
-            <div class="inputCount">{{content.length}}/200</div>
+          <div class="inputCount">{{content.length}}/200</div>
         </div>
         <div v-else style="height: 500rpx">
-          aaa
+          被发现了，下期支持吧
         </div>
       </div>
       <view class="cu-form-group margin-top">
         <view class='title'>匿名发布</view>
-        <switch></switch>
+        <switch @change="handleSwitchChange"></switch>
       </view>
       <!--发布-->
-      <button class='cu-btn block line-orange lg margin-top' @click="publishContent">
-        <text class='icon-upload'></text>
-        发布
-      </button>
+      <div class="padding">
+        <button class='cu-btn block bg-green lg margin-top' @click="publishContent">
+          <text class='icon-upload'></text>
+          发布
+        </button>
+      </div>
     </div>
-    <div v-else>
-      <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="onGotUserInfo">获取用户信息</button>
+    <div v-else class="user-login-container">
+      <button open-type="getUserInfo" @getuserinfo="onGotUserInfo">
+        <div class='user-avatar'>
+          <img src="../../../static/images/avatar.png" alt="">
+        </div>
+        <div class="tip">点击登录</div>
+      </button>
     </div>
   </div>
 </template>
@@ -59,6 +66,8 @@
         content: "",
         TabCur: "0",
         remnant: 200,
+        //是否匿名
+        isAnonymous: 0
       };
     },
     mounted() {
@@ -72,10 +81,6 @@
         console.log(e);
         let self = this;
         self.TabCur = e.currentTarget.dataset.id;
-        // this.setData({
-        //   TabCur: e.currentTarget.dataset.id,
-        //   scrollLeft: (e.currentTarget.dataset.id-1)*60
-        // })
       },
       //处理文本改变
       handleTextAreaChange(e) {
@@ -85,14 +90,34 @@
         self.remnant = 200 - e.target.value.length;
         self.content = e.target.value;
       },
+      //处理switch的改变
+      handleSwitchChange(e) {
+        console.log("change改变", e);
+        this.isAnonymous = e.mp.detail.value;
+      },
       //发布
       publishContent() {
         let self = this;
-        self.$http.postRequest("articles", {
-          content: self.content
-        }).then((res) => {
-          console.log("res", res);
-        });
+        if (self.content.length <= 15) {
+          wx.showToast({
+            title: "就这几个字,让我怎么交差",
+            icon: "none",
+            duration: 2000
+          });
+        } else {
+          self.$http.postRequest("articles", {
+            content: self.content,
+            anonymous: self.isAnonymous ? 1 : 0
+          }).then((res) => {
+            console.log("res", res);
+            self.content = "";
+            wx.showToast({
+              title: "发布成功",
+              icon: "success",
+              duration: 2000
+            });
+          });
+        }
       },
       // 进入查看用户是否授权
       getSetting() {
@@ -121,36 +146,18 @@
         if (e.mp.detail.rawData) {
           console.log("用户按下了允许");
           console.log(e.mp.detail);
-          self.isAuth = true;
+          self.$http.postRequest("users", {
+            iv: e.mp.detail.iv,
+            encryptData: e.mp.detail.encryptedData
+          }).then((res) => {
+            console.log("登录陈工", res);
+            self.isAuth = true;
+          });
         } else {
           console.log("用户按下拒绝");
           self.isAuth = false;
         }
       },
-      // onGotUserInfo(item) {
-      //   let self = this;
-      //   if (!mpvue.getStorageSync("nickName") || !mpvue.setStorageSync("avatarUrl")) {
-      //     wx.getUserInfo({
-      //       withCredentials: true,
-      //       success(res) {
-      //         console.log("获取的用户信息", res);
-      //         // self.userNickName = res.userInfo.nickName;
-      //         // self.userAvatarUrl = res.userInfo.avatarUrl;
-      //         mpvue.setStorageSync("nickName", res.userInfo.nickName);
-      //         mpvue.setStorageSync("avatarUrl", res.userInfo.avatarUrl);
-      //         self.$http.postRequest("users", {
-      //           iv: res.iv,
-      //           encryptData: res.encryptedData
-      //         }).then((res) => {
-      //           console.log("登录陈工", res);
-      //         });
-      //       }
-      //     });
-      //   } else {
-      //     // self.sharePic(item.id, self.userNickName, self.userAvatarUrl);
-      //     console.log(mpvue.getStorageSync("nickName"))
-      //   }
-      // },
       // 登录
       wxlogin() {
         let self = this;
@@ -187,11 +194,38 @@
     padding: 50rpx 25rpx;
     font-size: 30rpx;
   }
-  .inputCount{
+
+  .inputCount {
     position: absolute;
     top: 15rpx;
     right: 25rpx;
     font-size: 30rpx;
     color: gray
+  }
+
+  .user-login-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+
+  .user-avatar {
+    width: 200rpx;
+    height: 200rpx;
+    border-radius: 50%;
+    overflow: hidden;
+    display: block;
+    margin: 20rpx;
+    border: 2px solid #fff;
+    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+  }
+
+  .user-avatar img {
+    width: 100%;
+    height: 100%;
+  }
+  .tip{
+    color: gray;
   }
 </style>
