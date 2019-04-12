4d52bfa1c7c40a7193edddcc7dd2312c54a6f362
webwyb <beijingwyb@163.com>
2019-04-12 10:06:11 +0800

    feat: 注视无用代码

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,57 @@
 <script>
   export default {
-    created() {
-      // 调用API从本地缓存中获取数据
-      /*
-       * 平台 api 差异的处理方式:  api 方法统一挂载到 mpvue 名称空间, 平台判断通过 mpvuePlatform 特征字符串
-       * 微信：mpvue === wx, mpvuePlatform === 'wx'
-       * 头条：mpvue === tt, mpvuePlatform === 'tt'
-       * 百度：mpvue === swan, mpvuePlatform === 'swan'
-       * 支付宝(蚂蚁)：mpvue === my, mpvuePlatform === 'my'
-       */
-
-      let logs;
-      if (mpvuePlatform === "my") {
-        logs = mpvue.getStorageSync({ key: "logs" }).data || [];
-        logs.unshift(Date.now());
-        mpvue.setStorageSync({
-          key: "logs",
-          data: logs
-        });
-      } else {
-        logs = mpvue.getStorageSync("logs") || [];
-        logs.unshift(Date.now());
-        mpvue.setStorageSync("logs", logs);
-      }
-      // wx.login({
-      //   success(res) {
-      //     if (res.code) {
-      //       // 发起网络请求
-      //       console.log("发起网络请求", res.code);
-      //       console.log("发起网络请求", res);
-      //       wx.getUserInfo({
-      //         withCredentials: true,
-      //         success(res) {
-      //           console.log("userInfo", res);
-      //         }
-      //       });
-      //       wx.request({
-      //         url: "https://duanzi.fengtianhe.cn/api/uses/login",
-      //         data: {
-      //           code: res.code
-      //         }
-      //       });
-      //       // self.$http.get("/uses/login", {
-      //       //   code: res.code
-      //       // }, (res) => {
-      //       //   console.log(res);
-      //       // });
-      //     } else {
-      //       console.log("登录失败！" + res.errMsg);
-      //     }
-      //   }
-      // });
-    },
+    // created() {
+    //   // 调用API从本地缓存中获取数据
+    //   /*
+    //    * 平台 api 差异的处理方式:  api 方法统一挂载到 mpvue 名称空间, 平台判断通过 mpvuePlatform 特征字符串
+    //    * 微信：mpvue === wx, mpvuePlatform === 'wx'
+    //    * 头条：mpvue === tt, mpvuePlatform === 'tt'
+    //    * 百度：mpvue === swan, mpvuePlatform === 'swan'
+    //    * 支付宝(蚂蚁)：mpvue === my, mpvuePlatform === 'my'
+    //    */
+    //
+    //   let logs;
+    //   if (mpvuePlatform === "my") {
+    //     logs = mpvue.getStorageSync({ key: "logs" }).data || [];
+    //     logs.unshift(Date.now());
+    //     mpvue.setStorageSync({
+    //       key: "logs",
+    //       data: logs
+    //     });
+    //   } else {
+    //     logs = mpvue.getStorageSync("logs") || [];
+    //     logs.unshift(Date.now());
+    //     mpvue.setStorageSync("logs", logs);
+    //   }
+    //  wx.login({
+    //    success(res) {
+    //      if (res.code) {
+    //        // 发起网络请求
+    //        console.log("发起网络请求", res.code);
+    //        console.log("发起网络请求", res);
+    //        wx.getUserInfo({
+    //          withCredentials: true,
+    //          success(res) {
+    //            console.log("userInfo", res);
+    //          }
+    //        });
+    //        wx.request({
+    //          url: "https://duanzi.fengtianhe.cn/api/uses/login",
+    //          data: {
+    //            code: res.code
+    //          }
+    //        });
+    //         // self.$http.get("/uses/login", {
+    //         //   code: res.code
+    //         // }, (res) => {
+    //         //   console.log(res);
+    //         // });
+    //       } else {
+    //         console.log("登录失败！" + res.errMsg);
+    //       }
+    //     }
+    //   });
+    // },
     // onLaunch() {
     //   let self = this;
     //   self.$http.get("/uses/login", {
@@ -60,9 +60,9 @@
     //     console.log(res);
     //   });
     // },
-    log() {
-      console.log(`log at:${Date.now()}`);
-    }
+    // log() {
+    //   console.log(`log at:${Date.now()}`);
+    // }
   };
 </script>
 
@@ -73,7 +73,7 @@
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
-    padding: 200 rpx 0;
+    padding: 200rpx 0;
     box-sizing: border-box;
   }
 
